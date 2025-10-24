41c3e6251e98d788167d585aaa133c06a0d2d9a1
Agoa Elizabeth <elizabethagoa@gmail.com>
2025-10-24 06:38:23 +0300

    updated the build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,15 @@
 set -o errexit
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+set -o errexit
+
+echo "Starting build process..."
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Build complete!"
